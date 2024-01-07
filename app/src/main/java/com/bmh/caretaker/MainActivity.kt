49e2d7331e376ba1e2c24d0bc9949175ec7fd5de
faryz-coder <faryz.coder@gmail.com>
2024-01-07 19:48:13 +0800

    add app bar

--- app/src/main/java/com/bmh/caretaker/MainActivity.kt
+++ app/src/main/java/com/bmh/caretaker/MainActivity.kt
@@ -6,11 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -26,22 +40,68 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         setContent {
-            val interactionSource = remember { MutableInteractionSource() }
-            val focusManager = LocalFocusManager.current
-
             CaretakerTheme {
                 navHostController = rememberNavController()
                 mainViewModel.navController = navHostController
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clickable(interactionSource = interactionSource, indication = null) {
-                            focusManager.clearFocus()
-                        }, color = MaterialTheme.colorScheme.background
-                ) {
-                    MainNavGraph(mainViewModel = mainViewModel)
-                }
+
+                MainBody(mainViewModel)
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainBody(mainViewModel: MainViewModel) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val focusManager = LocalFocusManager.current
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Caretaker")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Rounded.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Rounded.MoreVert,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+            )
+        }
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .clickable(interactionSource = interactionSource, indication = null) {
+                    focusManager.clearFocus()
+                }, color = MaterialTheme.colorScheme.background
+        ) {
+            MainNavGraph(mainViewModel = mainViewModel)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMain() {
+    val mainViewModel = MainViewModel()
+    mainViewModel.navController  = rememberNavController()
+    MainBody(mainViewModel = mainViewModel)
 }
