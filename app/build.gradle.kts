b9e77200552e5dcaabc6b07840e9927add3fd8ed
faryz-coder <faryz.coder@gmail.com>
2024-01-14 22:43:34 +0800

    integrate with firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -64,6 +65,13 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     // Navigation
     implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
 
 
 
